4a5012ed80839241fbdbdfb29af8cb8a237ce62a
Thomas-Greaux <thomas.greaux@etu.unice.fr>
2018-04-28 10:15:56 +0100

    Simple SOAP Service, service not recognized as such...

---
+++ SOAPService/MathLibrary/ConsoleClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using MathLibrary;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            MathsOperationsClient client = MathsOperationsClient();
+            Console.WriteLine(client.Add(100, 11));
+            Console.WriteLine(client.Mult(47, 10));
+            */
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SOAPService/MathLibrary/MathLibrary/IMathsOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        int Add(int num1, int num2);
+
+        [OperationContract]
+        int Mult(int num1, int num2);
+    }
+}
