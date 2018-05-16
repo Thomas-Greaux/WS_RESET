86ac23726637b099cbd3b38ac12a072be9b1cdba
Thomas-Greaux <thomas.greaux@etu.unice.fr>
2018-05-16 21:58:36 +0100

    Premiere version de la GUI

---
+++ VelibProject/GUIClient/ClientForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GUIClient
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.bikes_nb_text_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Send_request);
+            // 
+            // bikes_nb_text_box
+            // 
+            this.bikes_nb_text_box.Location = new System.Drawing.Point(64, 187);
+            this.bikes_nb_text_box.Name = "bikes_nb_text_box";
+            this.bikes_nb_text_box.Size = new System.Drawing.Size(185, 22);
+            this.bikes_nb_text_box.TabIndex = 1;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.bikes_nb_text_box);
+            this.Controls.Add(this.button1);
+            this.Name = "ClientForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox bikes_nb_text_box;
+    }
+}
+
---
+++ VelibProject/GUIClient/ClientForm.cs
@@ -0,0 +1,34 @@
+﻿using GUIClient.IWSReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIClient
+{
+    public partial class ClientForm : Form
+    {
+        static IWSReference.IWSClient client = new IWSReference.IWSClient();
+
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Send_request(object sender, EventArgs e)
+        {
+            Station res = client.GetStation("Toulouse", "00189");
+            bikes_nb_text_box.Text = "Bikes available : " + res.Available_bikes;
+        }
+    }
+}
